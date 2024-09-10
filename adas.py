21ef3a15ccd03f59c0f70f1275330c74baf714d0
ethansmith2000 <ethansmith2000@gmail.com>
2024-09-10 02:43:18 -0400

    fix up ads

--- adas.py
+++ adas.py
@@ -1,191 +1,165 @@
 import torch
 from torch import nn
-from .sparse_layers import AlmostMonarch, MothMatrix
 import torch.nn.functional as F
+from .sparse_layers import AlmostMonarch
 
 
-class HyperAttentionLinear(nn.Module):
-
+class AdaLoRA(nn.Module):
     """
-    HyperAttention. Do perceiver attention to create weight matrix that is (dim*2 x dim), chunk it
-    then use it as an MLP with activation to inputs
+    Feels like an AdaNorm kinda layer, but condition is projected into a LoRA which hidden states are pushed through
+
+    shout out to rami_mmo for the initial design and idea!
     """
 
-    def __init__(self, x_dim_in, x_dim_out, hidden_attn_dim=None, kv_dim=None, heads=8):
+    def __init__(self, feat_dim, ada_dim, inter_dim=None, rank=8, act_fn=nn.GELU, sparse_heads=None):
         super().__init__()
-        self.h = heads
-        # if no external condition, x will be the input used to create the weight matrix
-        kv_dim = kv_dim or x_dim_in
-        # if no hidden_dim, use x_dim_out
-        hidden_attn_dim = hidden_attn_dim or x_dim_out
-        self.head_dim = hidden_attn_dim // heads
+        self.rank = rank
+        layers = []
+        inter_dim = ada_dim if inter_dim is None else inter_dim
+        if act_fn is None:
+            layers.append(nn.Linear(ada_dim, feat_dim * rank * 2))
+        else:
+            layers.append(nn.Linear(ada_dim, inter_dim))
+            layers.append(act_fn())
+            layers.append(
+                AlmostMonarch(inter_dim, feat_dim * rank * 2, sparse_heads) if sparse_heads is not None else nn.Linear(
+                    inter_dim, feat_dim * rank * 2))
 
-        # a learnable weight matrix, is used as queries and is combined with the attention output to give final weight matrix
-        base_weight = torch.empty(1, x_dim_in, x_dim_out)
-        torch.nn.init.kaiming_uniform_(base_weight)
-        self.base_weight = torch.nn.Parameter(base_weight)
+        self.gen_weight = nn.Sequential(*layers)
 
-        # attention weights
-        self.q_proj = nn.Linear(x_dim_out, hidden_attn_dim, bias=False)
-        self.kv_norm = nn.LayerNorm(kv_dim)
-        self.kv_proj = nn.Linear(kv_dim, hidden_attn_dim * 2, bias=False)
-        self.to_out = nn.Linear(hidden_attn_dim, x_dim_out)
+    def forward(self, x, ada_emb):
+        x_in = x
+        B, D = x.shape[0], x.shape[-1]
+        x_weights = self.gen_weight(ada_emb)
+        x_a, x_b = x_weights.chunk(2, dim=-1)
+        x_a, x_b = map(lambda t: t.reshape(-1, D, self.rank), (x_a, x_b))
 
+        x = torch.einsum('bc,bco->bo', x, x_a)
+        x = torch.einsum('bc,bco->bo', x, x_b.permute(0, 2, 1))
 
-    def forward(self, x, context=None):
-        # we can use external context or the input itself to guide the weight matrix creation
-        context = context if context is not None else x
-        B, N, Ci = context.shape
-        hd = self.q_proj.out_features
-        L, Co = self.base_weight.shape[1:]
-        base_weight = self.base_weight.expand(B, -1, -1)
-
-        # base weight serves as queries
-        queries = self.q_proj(base_weight)
-        norm_kv = self.kv_norm(context)
-        k, v = map(lambda t: t.reshape(B, N, self.h, self.head_dim).transpose(1, 2), self.kv_proj(norm_kv).chunk(2, dim=-1))
-        q = queries.reshape(B, L, self.h, self.head_dim).transpose(1, 2)
-
-        attn_out = F.scaled_dot_product_attention(q, k, v).transpose(1, 2).reshape(B, L, hd)
-        attn_out = self.to_out(attn_out)
-
-        # b, c, c_out
-        layer = base_weight + attn_out
-
-        # apply our new layer to x
-        x = torch.einsum('bnd,bdr->bnr', x, layer)
-
-        return x
+        # add back residual otherwise we kill the rank of our representation
+        return x + x_in
 
 
-
-class HyperAttentionMLP(nn.Module):
-
+class AdaLoRAMLP(nn.Module):
     """
-    HyperAttention. Do perceiver attention to create weight matrix that is (dim*2 x dim), chunk it
-    then use it as an MLP with activation to inputs
+    Feels like an AdaNorm kinda layer, but condition is projected into a LoRA which hidden states are pushed through
     """
 
-    def __init__(self, x_dim, hidden_dim, hidden_attn_dim=None, kv_dim=None, heads=8):
+    def __init__(self, feat_dim, ada_dim, inter_dim=None, rank=8, act_fn=nn.GELU, sparse_heads=None):
         super().__init__()
-        self.h = heads
-        # if no external condition, x will be the input used to create the weight matrix
-        kv_dim = kv_dim or x_dim
-        # if no hidden_dim, use x_dim_out
-        hidden_attn_dim = hidden_attn_dim or x_dim
-        self.head_dim = hidden_attn_dim // heads
+        self.rank = rank
+        layers = []
+        inter_dim = ada_dim if inter_dim is None else inter_dim
+        if act_fn is None:
+            layers.append(nn.Linear(ada_dim, feat_dim * rank * 4))
+        else:
+            layers.append(nn.Linear(ada_dim, inter_dim))
+            layers.append(act_fn())
+            layers.append(
+                AlmostMonarch(inter_dim, feat_dim * rank * 4, sparse_heads) if sparse_heads is not None else nn.Linear(
+                    inter_dim, feat_dim * rank * 4))
 
-        base_up = torch.randn(1, x_dim, hidden_dim)
-        base_down = torch.randn(1, hidden_dim, x_dim)
-        torch.nn.init.kaiming_uniform_(base_up)
-        torch.nn.init.kaiming_uniform_(base_down)
+        self.gen_weight = nn.Sequential(*layers)
+        self.act_fn = act_fn()
 
-        base_weight = torch.cat([base_down, base_up.transpose(1, 2)], dim=1)
-        self.base_weight = torch.nn.Parameter(base_weight)
+    def forward(self, x, ada_emb):
+        x_in = x
+        B, D = x.shape[0], x.shape[-1]
+        x_weights = self.gen_weight(ada_emb)
+        x_a_1, x_b_1, x_a_2, x_b_2 = x_weights.chunk(4, dim=-1)
+        x_a_1, x_b_1, x_a_2, x_b_2 = map(lambda t: t.reshape(-1, D, self.rank), (x_a_1, x_b_1, x_a_2, x_b_2))
 
-        self.q_proj = nn.Linear(x_dim, hidden_attn_dim, bias=False)
-        self.kv_norm = nn.LayerNorm(kv_dim)
-        self.kv_proj = nn.Linear(kv_dim, hidden_attn_dim * 2, bias=False)
-        self.to_out = nn.Linear(hidden_attn_dim, x_dim)
-
-        self.act_fn = nn.GELU()
-
-    def forward(self, x, context=None):
-        # we can use external context or the input itself to guide the weight matrix creation
-        context = context if context is not None else x
-        B, N, Ci = context.shape
-        L = self.base_weight.shape[1]
-        hd = self.q_proj.out_features
-        base_weight = self.base_weight.expand(B, -1, -1)
-
-        # base weight serves as queries
-        queries = self.q_proj(base_weight)
-        norm_kv = self.kv_norm(context)
-        k, v = map(lambda t: t.reshape(B, N, self.h, self.head_dim).transpose(1, 2), self.kv_proj(norm_kv).chunk(2, dim=-1))
-        q = queries.reshape(B, L, self.h, self.head_dim).transpose(1, 2)
-
-        attn_out = F.scaled_dot_product_attention(q, k, v).transpose(1, 2).reshape(B, L, hd)
-        attn_out = self.to_out(attn_out)
-
-        # b, ff_dim * 2, c_out
-        layer = base_weight + attn_out
-
-        # chunk to get up and down
-        up, down = layer.chunk(2, dim=1)
-
-        # apply our new layer to x
-        x = torch.einsum('bnd,bld->bnl', x, up)
+        x = torch.einsum('bc,bco->bo', x, x_a_1)
+        x = torch.einsum('bc,bco->bo', x, x_b_1.permute(0, 2, 1))
         x = self.act_fn(x)
-        x = torch.einsum('bnd,bld->bnl', x, down.transpose(1, 2))
+        x = torch.einsum('bc,bco->bo', x, x_a_2)
+        x = torch.einsum('bc,bco->bo', x, x_b_2.permute(0, 2, 1))
 
-        return x
+        # add back residual otherwise we kill the rank of our representation
+        return x + x_in
 
 
-class HyperAttentionAttention(nn.Module):
-
+class AdaLoRAWithBase(nn.Module):
     """
-    HyperAttention. Do perceiver attention to create query, key, value, and out matrices
-    then use that for attention
-    if you're a bit unhinged, this could technically be done recursively forever until you OOM
+    Feels like an AdaNorm kinda layer, but condition is projected into a LoRA which hidden states are pushed through
     """
 
-    def __init__(self, x_dim, hidden_dim, hidden_attn_dim=None, kv_dim=None, heads=8):
+    def __init__(self, feat_dim, ada_dim, inter_dim=None, rank=8, act_fn=nn.GELU, sparse_heads=None, residual=True):
         super().__init__()
-        self.h = heads
-        # if no external condition, x will be the input used to create the weight matrix
-        kv_dim = kv_dim or x_dim
-        # if no hidden_dim, use x_dim_out
-        hidden_attn_dim = hidden_attn_dim or x_dim
-        self.head_dim = hidden_attn_dim // heads
+        self.rank = rank
+        self.base_layer = nn.Parameter(torch.randn(feat_dim, feat_dim))
+        layers = []
+        inter_dim = ada_dim if inter_dim is None else inter_dim
+        if act_fn is None:
+            layers.append(nn.Linear(ada_dim, feat_dim * rank * 2))
+        else:
+            layers.append(nn.Linear(ada_dim, inter_dim))
+            layers.append(act_fn())
+            layers.append(
+                AlmostMonarch(inter_dim, feat_dim * rank * 2, sparse_heads) if sparse_heads is not None else nn.Linear(
+                    inter_dim, feat_dim * rank * 2))
 
-        query = torch.randn(1, x_dim, hidden_dim)
-        key = torch.randn(1, x_dim, hidden_dim)
-        value = torch.randn(1, x_dim, hidden_dim)
-        to_out = torch.randn(1, hidden_dim, x_dim)
-        torch.nn.init.kaiming_uniform_(query)
-        torch.nn.init.kaiming_uniform_(key)
-        torch.nn.init.kaiming_uniform_(value)
-        torch.nn.init.kaiming_uniform_(to_out)
+        self.gen_weight = nn.Sequential(*layers)
 
-        base_weight = torch.cat([query, key, value, to_out.transpose(1, 2)], dim=1)
-        self.base_weight = torch.nn.Parameter(base_weight)
+        self.residual = lambda x, y: x + y if residual else y
 
-        self.q_proj = nn.Linear(hidden_dim, hidden_attn_dim, bias=False)
-        self.kv_norm = nn.LayerNorm(kv_dim)
-        self.kv_proj = nn.Linear(kv_dim, hidden_attn_dim * 2, bias=False)
-        self.to_out = nn.Linear(hidden_attn_dim, hidden_dim)
+    def forward(self, x, ada_emb):
+        x_in = x
+        B, D = x.shape[0], x.shape[-1]
+        x_weights = self.gen_weight(ada_emb)
+        x_a, x_b = x_weights.chunk(2, dim=-1)
+        x_a, x_b = map(lambda t: t.reshape(-1, D, self.rank), (x_a, x_b))
 
-        self.act_fn = nn.GELU()
+        # fuse
+        layer = torch.einsum('bdr,brk->bdk', x_a, x_b.transpose(1, 2))
+        layer = self.base_layer[None, ...] + layer
 
-    def forward(self, x, context=None):
-        # we can use external context or the input itself to guide the weight matrix creation
-        context = context if context is not None else x
-        B, N, Ci = context.shape
-        L = self.base_weight.shape[1]
-        hd = self.q_proj.out_features
-        base_weight = self.base_weight.expand(B, -1, -1)
+        x = torch.einsum('bc,bco->bo', x, layer)
 
-        # base weight serves as queries
-        queries = self.q_proj(base_weight)
-        norm_kv = self.kv_norm(context)
-        k, v = map(lambda t: t.reshape(B, N, self.h, self.head_dim).transpose(1, 2), self.kv_proj(norm_kv).chunk(2, dim=-1))
-        q = queries.reshape(B, L, self.h, self.head_dim).transpose(1, 2)
+        return self.residual(x_in, x)
 
-        attn_out = F.scaled_dot_product_attention(q, k, v).transpose(1, 2).reshape(B, L, hd)
-        attn_out = self.to_out(attn_out)
 
-        # b, dim * 4, c_out
-        layer = base_weight + attn_out
+class AdaLoRAMLPWithBase(nn.Module):
+    """
+    Feels like an AdaNorm kinda layer, but condition is projected into a LoRA which hidden states are pushed through
+    """
 
-        # chunk to get up and down
-        q_proj, k_proj, v_proj, o_proj = layer.chunk(4, dim=1)
+    def __init__(self, feat_dim, ada_dim, inter_dim=None, rank=8, act_fn=nn.GELU, sparse_heads=None, residual=True):
+        super().__init__()
+        self.rank = rank
+        layers = []
+        self.base_up = nn.Parameter(torch.randn(feat_dim, feat_dim))
+        self.base_down = nn.Parameter(torch.randn(feat_dim, feat_dim))
+        inter_dim = ada_dim if inter_dim is None else inter_dim
+        if act_fn is None:
+            layers.append(nn.Linear(ada_dim, feat_dim * rank * 4))
+        else:
+            layers.append(nn.Linear(ada_dim, inter_dim))
+            layers.append(act_fn())
+            layers.append(
+                AlmostMonarch(inter_dim, feat_dim * rank * 4, sparse_heads) if sparse_heads is not None else nn.Linear(
+                    inter_dim, feat_dim * rank * 4))
 
-        # apply our new layer to x
-        q2 = torch.einsum('bnd,bdl->bnl', x, q_proj)
-        k2 = torch.einsum('bnd,bdl->bnl', x, k_proj)
-        v2 = torch.einsum('bnd,bdl->bnl', x, v_proj)
+        self.gen_weight = nn.Sequential(*layers)
+        self.act_fn = act_fn()
 
-        out = F.scaled_dot_product_attention(q2, k2, v2)
-        out = torch.einsum('bnl,blc->bnc', out, o_proj.transpose(1, 2))
+        self.residual = lambda x, y: x + y if residual else y
 
-        return out
+    def forward(self, x, ada_emb):
+        x_in = x
+        B, D = x.shape[0], x.shape[-1]
+        x_weights = self.gen_weight(ada_emb)
+        x_a_1, x_b_1, x_a_2, x_b_2 = x_weights.chunk(4, dim=-1)
+        x_a_1, x_b_1, x_a_2, x_b_2 = map(lambda t: t.reshape(-1, D, self.rank), (x_a_1, x_b_1, x_a_2, x_b_2))
+
+        x_up = torch.einsum('bdr,brk->bdk', x_a_1, x_b_1.transpose(1, 2))
+        x_down = torch.einsum('bdr,brk->bdk', x_a_2, x_b_2.transpose(1, 2))
+
+        x_up = self.base_up[None, ...] + x_up
+        x_down = self.base_down[None, ...] + x_down
+
+        x = torch.einsum('bdl,bd->bl', x_down, x)
+        x = torch.einsum('bkl,bl->bk', x_up, x)
+
+        return self.residual(x_in, x)
